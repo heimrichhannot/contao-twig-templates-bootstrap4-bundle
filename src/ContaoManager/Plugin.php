2e263d6e1a5da6dc4c47d9d8cdd0b9317eb885b2
Thomas Körner <t.koerner@heimrich-hannot.de>
2021-05-12 12:09:45 +0200

    inital commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Copyright (c) 2021 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\TwigTemplatesBootstrap4Bundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use HeimrichHannot\TwigTemplatesBootstrap4Bundle\HeimrichHannotTwigTemplatesBootstrap4Bundle;
+use HeimrichHannot\TwigTemplatesBundle\HeimrichHannotTwigTemplatesBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class Plugin implements BundlePluginInterface, ConfigPluginInterface
+{
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(HeimrichHannotTwigTemplatesBootstrap4Bundle::class)->setLoadAfter([
+                ContaoCoreBundle::class,
+                HeimrichHannotTwigTemplatesBundle::class,
+            ]),
+        ];
+    }
+
+    /**
+     * Allows a plugin to load container configuration.
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
+    {
+        $loader->load('@HeimrichHannotTwigTemplatesBootstrap4Bundle/Resources/config/services.yml');
+    }
+}
